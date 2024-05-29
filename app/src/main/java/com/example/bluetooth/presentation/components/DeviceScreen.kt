4840138daa0cdeb3f3c24309c00aa8e87b896597
İsmail Fatih Birak <ismailfatihbirak@gmail.com>
2024-05-29 13:48:34 +0300

    second

--- app/src/main/java/com/example/bluetooth/presentation/components/DeviceScreen.kt
+++ app/src/main/java/com/example/bluetooth/presentation/components/DeviceScreen.kt
@@ -23,7 +23,9 @@ import com.example.bluetooth.presentation.BluetoothUiState
 fun DeviceScreen(
     state: BluetoothUiState,
     onStartScan: () -> Unit,
-    onStopScan: () -> Unit
+    onStopScan: () -> Unit,
+    onStartServer: () -> Unit,
+    onDeviceClick: (BluetoothDevice) -> Unit
 ) {
     Column(
         modifier = Modifier
@@ -32,7 +34,7 @@ fun DeviceScreen(
         BluetoothDeviceList(
             pairedDevices = state.pairedDevices,
             scannedDevices = state.scannedDevices,
-            onClick = {},
+            onClick = onDeviceClick,
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(1f)
@@ -46,6 +48,9 @@ fun DeviceScreen(
             }
             Button(onClick = onStopScan) {
                 Text(text = "Stop scan")
+            }
+            Button(onClick = onStartServer) {
+                Text(text = "Start server")
             }
         }
     }
